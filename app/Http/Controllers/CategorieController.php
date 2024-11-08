ef016d72e79e812c39c6304fa3d0fb8572206dd4
idn-fahmifn <fahmifn.idn@gmail.com>
2024-11-08 13:33:42 +0700

    first

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Categorie::all();
+        return view('kategori.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        // dd($data);
+        Categorie::create($data);
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return view('kategori.detail');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $data = Categorie::find($id);
+        $data->delete();
+        return back();
+    }
+}
---
+++ resources/views/kategori/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <form action="{{route('categories.store')}}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label class="form-label">Nama Kategori</label>
+                            <input type="text" name="kategori" class="form-control">
+                        </div>
+                        <div class="form-group mt-4 mb-4">
+                            <button type="submit" class="btn btn-primary">Tambah Kategori</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <th>Nama Kategori</th>
+                                <th>Pilihan</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $item)
+                                <tr>
+                                    <td>{{$item->kategori}}</td>
+                                    <td>
+                                        <form action="{{route('categories.destroy', $item->id)}}" method="post">
+                                            @csrf
+                                            {{method_field('delete')}}
+
+                                            <button type="submit" class="btn btn-danger">Hapus</button>
+                                            <a href="{{route('categories.show', $item->id)}}" class="btn btn-info">Detail</a>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
